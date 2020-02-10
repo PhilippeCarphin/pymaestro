f02badb6061a2a26eae03c4e6b0157f3bb780e21
Philippe Carphin <philippe.carphin@polymtl.ca>
2020-02-10 10:08:34 -0500

    WIP

--- node.py
+++ node.py
@@ -17,7 +17,7 @@ tag_to_enum = {
     "LOOP": NodeType.LOOP,
     "SWITCH": NodeType.SWITCH,
     "TASK": NodeType.TASK,
-    "NPASSTASK": NodeType.NPASSTASK,
+    "NPASS_TASK": NodeType.NPASSTASK,
     "FOREACH": NodeType.FOREACH
 }
 def get_node_type(tag):
@@ -27,9 +27,56 @@ class PathTokenError(Exception):
     pass
 
 class FlowVisitor:
-    def __init__(self, exp_home):
+    def __init__(self, exp_home, node_function=None):
         self.xml_context = ET.parse(f'{exp_home}/EntryModule/flow.xml')
+        self.current_flow_node = self.xml_context.getroot()
+        self.current_node_type = get_node_type(self.current_flow_node.tag)
+        self.exp_home = exp_home
+        self.depth = 0
+    def node_function(self, node, path):
+        self.depth_print(f"{node} (name = '{node.attrib['name']}, path = {path})")
+    def depth_print(self, message):
+        print("    "* self.depth + str(message))
+    def visit_flow(self):
+        self.visit_node(self.xml_context.getroot(), '')
+    def visit_node(self, node, path):
+        self.depth += 1
+        for child in node.findall('*[@name]'):
+            path += "/" + child.attrib['name']
+            node_type = get_node_type(child.tag)
+            if node_type is NodeType.MODULE:
+                new_root = self.visit_module(child)
+                self.node_function(new_root, path)
+                self.visit_node(new_root, path)
+            elif node_type is NodeType.SWITCH:
+                switch_item = self.visit_switch(child)
+                self.node_function(switch_item, path)
+                self.visit_node(switch_item, path)
+            elif node_type is NodeType.LOOP:
+                self.node_function(child, path)
+                self.visit_node(child, path)
+            else:
+                self.node_function(child, path)
+                self.visit_node(child, path)
+        self.depth -= 1
+    def visit_switch(self, switch):
+        """ Just enter the first switch item for now """
+        switch_type = switch.attrib['type']
+        for d in switch:
+            if True:
+                return d
+    def visit_module(self, child):
+        module_name = child.attrib['name']
+        new_xml_filename = f'{self.exp_home}/modules/{module_name}/flow.xml'
+        new_xml_context = ET.parse(new_xml_filename)
+        new_root = new_xml_context.getroot()
+        return new_root
 
+if __name__ == "__main__":
+    v = FlowVisitor(os.getcwd() + '/experiments/sample_exp/')
+    v.visit_flow()
+    quit()
+    
 
 class NodeInfoHelper:
     def __init__(self, node_path, exp_home, switch_args=None):
